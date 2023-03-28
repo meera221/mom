c1fded72a1fe47a7742480135b56cdf832253688
meera221 <129191419+meera221@users.noreply.github.com>
2023-03-28 19:33:32 +0530

    Add files via upload

---
+++ mom.py
@@ -0,0 +1,143 @@
+from playsound import playsound
+import  vlc
+import time
+i=0
+while i<=90:
+    tambola=int(input("enter the number "))
+    i=i+1
+    if tambola ==1:
+
+    
+        playsound('songs/raatan lambiyan.mp3')
+       
+    if tambola ==2:
+
+    
+        playsound('songs/Kesariya tera ishq hai piya.mp3')
+        
+    if tambola ==3:
+
+    
+        playsound('songs/Neele Neele Ambar Par.mp3')
+          
+    if tambola ==4:
+
+    
+        playsound('songs/Yeh Chand Sa Roshan Chehra .mp3')
+    if tambola ==5:
+
+    
+        playsound('songs/jumma chumma de de chumma.mp3')
+    if tambola ==6:
+
+    
+        playsound('songs/tujhe dekha to yee janaa Sanam.mp3')
+    if tambola ==7:
+
+    
+        playsound('songs/PYAR HUA IKRAR HUA.mp3')
+    if tambola ==8:
+
+    
+        playsound('songs/Mera naam Chin chin Chu.mp3')
+    if tambola ==9:
+        playsound('songs/Chura liya hai tumne jo dil ')
+    if tambola ==10:
+        playsound('songs/Chahe Koi Mujhe Jungalee Kahe.mp3')
+    if tambola ==11:
+        playsound('songs/Gunguna rahe hain bhanwre.mp3')
+    if tambola ==12:
+        playsound('songs/jawani janeman.mp3')
+    if tambola ==13:
+        playsound('songs/Desi Girl.mp3')
+    if tambola ==14:
+        playsound('songs/Piya tu ab to aaja.mp3')
+    if tambola ==15:
+       playsound('songs/Zindagi Ek Safar Hai.mp3')
+    if tambola ==16:
+        playsound('songs/aye meri Zohra jabeen .mp3')
+    if tambola ==17:
+        playsound('songs/Tere mast mast do nain.mp3')
+    if tambola ==18:
+        playsound('songs/Aap Jaisa Koi Meri Zindagi.mp3')
+    if tambola ==19:
+        playsound('songs/Roop Tera Mastana.mp3')
+    if tambola ==20:
+        playsound('songs/Aaja Sanam Madhur Chandni.mp3')
+    if tambola ==21:
+        playsound('songs/kajra Re.mp3')
+    if tambola ==22:
+        playsound('songs/pehle ka nata koi.mp3')
+    if tambola ==23:
+        playsound('songs/Ye Desh Hai Veer Jawano Ka.mp3')
+    if tambola ==24:
+        playsound('songs/Phoolon Ke Rang Se.mp3')
+    if tambola ==25:
+        playsound('songs/Ek pyar ka nagma hai.mp3')
+    if tambola ==26:
+        playsound('songs/Bachana ae haseeno.mp3')
+    if tambola ==27:
+        playsound('songs/radha teri chunri.mp3')
+    if tambola ==28:
+        playsound('songs/badan  pe Sitare lapete hui.mp3')
+    if tambola ==29:
+        playsound('songs/Jaane Jaan Dhoondta.mp3')
+    if tambola ==30:
+        playsound('songs/ajib dastan hai.mp3')
+    if tambola ==31:
+        playsound('songs/Mere Sapno Ki Rani Kab.mp3')
+    if tambola ==32:
+        playsound('songs/Do Lafzon Ki Hai Dil Ki Kahani.mp3')
+    if tambola ==33:
+        playsound('songs/Elo ji Sanam.mp3')
+    if tambola ==34:
+        playsound('songs/Khaike Paan Banaras WaLa.mp3')
+    if tambola ==35:
+        playsound('songs/Jhoome Jo Pathaan.mp3')
+    if tambola ==36:
+        playsound('songs/Nagada sang dhol baaje.mp3')
+    if tambola ==37:
+        playsound('songs/Ek Do Teen.mp3')
+    if tambola ==38:
+            playsound('songs/Lungi Dance.mp3')
+    if tambola ==39:
+            playsound('songs/Kabhi kabhi Mere Dil Mein.mp3')
+    if tambola ==40:
+            playsound('songs/Mene Tere Liye Hi Saat Rang Ke Sapne Chune.mp3')
+    if tambola ==41:
+            playsound('songs/Saree ke fall sa.mp3')
+    if tambola ==42:
+            playsound('songs/Balam Pichkari.mp3')
+    if tambola ==43:
+            playsound('songs/Dilli Wali Girlfriend.mp3')
+    if tambola ==44:
+            playsound('songs/kabira maan ja.mp3')
+    if tambola ==45:
+            playsound('songs/In Aankhon Ki Masti.mp3')
+    if tambola ==46:
+            playsound('songs/Jeh tenu dhoop lageya ve.mp3')
+    if tambola ==47:
+            playsound('songs/Samne Yeh Kaun Aaya Dil Mein Hui Halchal.mp3')
+    if tambola ==48:
+            playsound('songs/Kuch Kuch Hota Hai .mp3')
+    if tambola ==49:
+            playsound('songs/Tujhe Dekha To Ye Jana Sanam.mp3')
+    if tambola ==50:
+            playsound('songs/Chalte Chalte Mere Ye Geet.mp3')
+    if tambola ==51:
+            playsound('songs/.mp3')
+    if tambola ==52:
+            playsound('songs/.mp3')
+    if tambola ==53:
+            playsound('songs/.mp3')
+    if tambola ==54:
+            playsound('songs/.mp3')
+    
+    
+    
+    
+            
+            
+            
+    
+    
